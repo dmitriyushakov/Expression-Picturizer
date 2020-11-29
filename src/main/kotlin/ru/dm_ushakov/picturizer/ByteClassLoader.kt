abb9e8f2ffb765466f4d61be72ccf0c51d2cafb2
dmitriyushakov <dmitryushakov1@yandex.ru>
2020-11-29 17:41:31 +0400

    Support extensions in package.

--- src/main/kotlin/ru/dm_ushakov/picturizer/ByteClassLoader.kt
+++ src/main/kotlin/ru/dm_ushakov/picturizer/ByteClassLoader.kt
@@ -4,7 +4,7 @@ import ru.dm_ushakov.picturizer.extensions.ExtensionsRegistry
 
 class ByteClassLoader(parent:ClassLoader):ClassLoader(parent) {
     override fun loadClass(name: String): Class<*> {
-        val bytecode = ExtensionsRegistry.extensionsList.find { it.className == name }?.bytecode
+        val bytecode = ExtensionsRegistry.extensionsList.find { it.className matchTo name }?.bytecode
         return if (bytecode != null) {
             defineClass(name,bytecode,0,bytecode.size)
         } else {
@@ -14,5 +14,17 @@ class ByteClassLoader(parent:ClassLoader):ClassLoader(parent) {
 
     fun getClassFromBytecode(name:String,buf:ByteArray):Class<*> {
         return defineClass(name,buf,0,buf.size)
+    }
+
+    infix fun String.matchTo(other:String):Boolean {
+        if(length != other.length) return false
+        for(i in 0 until length) {
+            val ch1 = this[i]
+            val ch2 = other[i]
+
+            if(!(ch1 == ch2 || (ch1 == '.' && ch2 == '/') || (ch1 == '/' && ch2 == '.'))) return false
+        }
+
+        return true
     }
 }
